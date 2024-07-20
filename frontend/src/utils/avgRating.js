54fbc99477ef2ca8b005e97c9481e5aad04b5907
PiyumalKK <piyumalm912@gmail.com>
2024-07-20 23:31:24 +0530

    avg Rating func

---
+++ frontend/src/utils/avgRating.js
@@ -0,0 +1,19 @@
+
+
+
+const calculateRating = reviews=>{
+    const totalRating = reviews?.reduce((acc,item) => acc+item.rating, 0);
+    const avgRating = 
+    totalRating === 0
+    ? ""
+    : totalRating === 1
+    ? totalRating
+    : totalRating / reviews?.length;
+
+    return{
+        totalRating,
+        avgRating
+    }
+}
+
+export default calculateRating;

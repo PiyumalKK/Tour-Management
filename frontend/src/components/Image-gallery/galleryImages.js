f51ac12bdf788361381c7be0778991cd341a26d7
PiyumalKK <piyumalm912@gmail.com>
2024-07-21 09:13:59 +0530

    Experience and gallery section

---
+++ frontend/src/components/Image-gallery/galleryImages.js
@@ -0,0 +1,20 @@
+import galleryImg01 from '../../assets/images/gallery-01.jpg';
+import galleryImg02 from '../../assets/images/gallery-02.jpg';
+import galleryImg03 from '../../assets/images/gallery-03.jpg';
+import galleryImg04 from '../../assets/images/gallery-04.jpg';
+import galleryImg05 from '../../assets/images/gallery-05.jpg';
+import galleryImg06 from '../../assets/images/gallery-06.jpg';
+import galleryImg07 from '../../assets/images/gallery-07.jpg';
+
+const galleryImagesData = [
+    { src: galleryImg01 },
+    { src: galleryImg02 },
+    { src: galleryImg03 },
+    { src: galleryImg04 },
+    { src: galleryImg05 },
+    { src: galleryImg06 },
+    { src: galleryImg07 },
+    { src: galleryImg01 },
+];
+
+export default galleryImagesData;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,10 +5,14 @@ import heroImg from '../assets/images/hero-img01.jpg';
 import heroImg02 from '../assets/images/hero-img02.jpg';
 import heroVideo from '../assets/images/hero-video.mp4';
 import worldImg from '../assets/images/world.png';
+import experienceImg from '../assets/images/experience.png';
 import Subtitle from '../shared/Subtitle';
 import SearchBar from '../shared/SearchBar';
 import ServiceList from '../services/ServiceList';
 import FeaturedTourList from '../components/Featured-tours/FeaturedTourList';
+import MasonryImagesGallery from '../components/Image-gallery/MasonryImagesGallery';
+
+
 
 export default function Home() {
   return (
@@ -82,10 +86,80 @@ export default function Home() {
   </Container>
 </section>
 
-        {/* Featured Tour Section Start */}
+        {/* Featured Tour Section End */}
+
+        {/* Experience Section Start */}
+
+<section>
+  <Container>
+    <Row>
+      <Col lg='6'>
+      <div className="experience__content">
+        <Subtitle subtitle={'Experience'}/>
+        <h2>With our all experience <br/> we will serve you</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus corporis quibusdam<br/>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum molestias sunt,  </p>
+      </div>
+      <div className="counter__wrapper d-flex align-items-center gap-5">
+        <div className="counter__box">
+          <span>
+            12k+
+          </span>
+          <h6>Successful Trip</h6>
+          
+        </div>
+        <div className="counter__box">
+          <span>
+            2k+
+          </span>
+          <h6>Regular Clients</h6>
+          
+        </div>
+        <div className="counter__box">
+          <span>
+            15
+          </span>
+          <h6>Years Experience</h6>
+          
+        </div>
+      </div>
+      </Col>
+      <Col lg='6'>
+      <div className="experience__img">
+        <img src={experienceImg} alt="" />
+      </div>
+      </Col>
+    </Row>
+  </Container>
+</section>
+
+ {/* Experience Section end */}
+  
+  {/* Gallery Section Start */}
+
+  <section>
+    <Container>
+      <Row>
+        <Col lg='12'>
+        <Subtitle subtitle={'Gallery'}/>
+        <h2 className='gallery__title'>Visite our customers tour gallery</h2>
+        </Col>
+        <Col lg='12'>
+        <MasonryImagesGallery/>
+        </Col>
+      </Row>
+    </Container>
+  </section>
 
 
+  {/* Gallery Section End */}
 
+  {/* Testimonial Section Start */}
+
+<section>
+  
+</section>
+
+   {/* Testimonial Section End */}
     </>
   )
 }

c770e4c6c9265dfaf4d369f9aa07bd4f49da07b9
PiyumalKK <piyumalm912@gmail.com>
2024-07-28 22:27:09 +0530

    Booking

--- frontend/src/components/Booking/Booking.jsx
+++ frontend/src/components/Booking/Booking.jsx
@@ -6,6 +6,10 @@ const Booking = ({tour, avgRating}) => {
 
 const {price,reviews} = tour
 
+const handleChange = () => {
+
+};
+
   return (
     <div className="booking">
         <div className="booking__top d-flex align-items-center justify-content-between">
@@ -17,8 +21,22 @@ const {price,reviews} = tour
                  </span>
 
         </div>
-     
-
+     {/* Booking Form */}
+       <div className="booking__form">
+        <h5>Information</h5>
+        <Form className='booking__info-form'>
+         <FormGroup>
+            <input type='text' placeholder='Full Name' id='fullname' required onChange={handleChange}/>
+         </FormGroup>
+         <FormGroup>
+            <input type='number' placeholder='Phone' id='phone' required onChange={handleChange}/>
+         </FormGroup>
+         <FormGroup className='d-flex align-items-center gap-3'>
+            <input type='date' placeholder='' id='bookAt' required onChange={handleChange}/>
+            <input type='number' placeholder='Guest' id='guestSize' required onChange={handleChange}/>
+         </FormGroup>
+        </Form>
+       </div>
     </div>
   )
 }
